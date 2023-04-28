85735698bad0e7037b9d76bcc02f320df915c649
inhokang1 <128698549+inhokang1@users.noreply.github.com>
2023-04-28 14:11:03 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -0,0 +1,12 @@
+n = int(input())
+d = [0]*100
+def fibo(x):
+    if x<3:
+        return 1
+    if d[x] != 0:
+        return d[x]
+    
+    d[x] = fibo(x-1)+fibo(x-2)
+    return d[x]
+
+print(fibo(n))
