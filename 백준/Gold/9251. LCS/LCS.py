34c44169ab798bf5c750775cc0b8b50fdd9897db
inhokang1 <128698549+inhokang1@users.noreply.github.com>
2023-04-29 23:05:41 +0900

    [Gold V] Title: LCS, Time: 544 ms, Memory: 55712 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,16 +1,19 @@
 import sys
+read = sys.stdin.readline
 
-S1 = sys.stdin.readline().strip().upper()
-S2 = sys.stdin.readline().strip().upper()
-len1 = len(S1)
-len2 = len(S2)
-matrix = [[0] * (len2 + 1) for _ in range(len1 + 1)]
+word1, word2 = read().strip(), read().strip()
+len1 = len(word1)
+len2 = len(word2)
+#메모이제이션 리스트
+dp = [[0]*(len2+1) for _ in range(len1+1)]
 
-for i in range(1, len1 + 1):
-    for j in range(1, len2 + 1):
-        if S1[i - 1] == S2[j - 1]:
-            matrix[i][j] = matrix[i - 1][j - 1] + 1
-        else:
-            matrix[i][j] = max(matrix[i - 1][j], matrix[i][j - 1])
+#글자 하나씩 서로 비교
+for i in range(1,len1+1):
+    for j in range(1,len2+1):
+        if word1[i-1] == word2[j-1]:
+            dp[i][j] = dp[i-1][j-1]+1
+        else :
+            dp[i][j] = max(dp[i-1][j],dp[i][j-1])
 
-print(matrix[-1][-1])
+#가장 큰 값 print
+print(dp[-1][-1])
