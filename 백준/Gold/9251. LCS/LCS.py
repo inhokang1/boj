53d82df4ea18fd3ab2075c8cef011b978da8803a
inhokang1 <128698549+inhokang1@users.noreply.github.com>
2023-04-29 23:06:38 +0900

    [Gold V] Title: LCS, Time: 524 ms, Memory: 55712 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,15 +1,14 @@
-import sys
-read = sys.stdin.readline
+from sys import stdin as s
 
-word1, word2 = read().strip(), read().strip()
-len1 = len(word1)
-len2 = len(word2)
+word1 = list(s.readline().rstrip())
+word2 = list(s.readline().rstrip())
+
 #메모이제이션 리스트
-dp = [[0]*(len2+1) for _ in range(len1+1)]
+dp = [[0]*(len(word2)+1) for _ in range(len(word1)+1)]
 
 #글자 하나씩 서로 비교
-for i in range(1,len1+1):
-    for j in range(1,len2+1):
+for i in range(1,len(word1)+1):
+    for j in range(1,len(word2)+1):
         if word1[i-1] == word2[j-1]:
             dp[i][j] = dp[i-1][j-1]+1
         else :
