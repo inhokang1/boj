378868069df3f11b3d33e0bd72b5efd2f9903c52
inhokang1 <128698549+inhokang1@users.noreply.github.com>
2023-05-17 09:38:19 +0900

    [Silver III] Title: 등수 매기기, Time: 536 ms, Memory: 52640 KB -BaekjoonHub

--- 백준/Silver/2012. 등수 매기기/등수 매기기.py
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.py
@@ -1,13 +1,18 @@
 import sys
 input = sys.stdin.readline
 
+#사람수
 n = int(input())
 
+#예상 등수 리스트로 받기
 a = []
 for _ in range(n):
     a.append(int(input()))
+#등수 정렬
 a.sort()
 
+#정렬된 리스트에서 각 숫자들이 자기숫자와 index+1 이 틀리다면 
+# 차이 만큼의 절대값을 cnt에 저장
 cnt = 0
 for i in range(len(a)):
     if a[i] == i+1:
