93b92dcfbb177b69a413ac3575c6ea5d086f1aba
inhokang1 <128698549+inhokang1@users.noreply.github.com>
2023-06-01 04:36:10 +0900

    [Silver III] Title: 1로 만들기, Time: 556 ms, Memory: 39068 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -9,13 +9,9 @@ for i in range(2,n+1):
     d[i] = d[i-1]+1
 
     if i%2 == 0:
-        a = d[i]
-        b = d[i//2]+1
         d[i] = min(d[i],d[i//2]+1)
 
     if i%3 == 0:
-        c = d[i]
-        f = d[i//3]+1
         d[i] = min(d[i],d[i//3]+1)
 
 print(d[n])
